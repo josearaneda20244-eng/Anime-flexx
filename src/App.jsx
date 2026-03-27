ca099f97cac4db1649da8df80e99053cf4378d98
ZeroIndent <josearaneda20244@gmail.com>
2026-03-27 09:29:08 -0300

    Replace TelegramModal with DiscordModal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import TelegramModal from './components/TelegramModal';
-// ...existing code...
+// Cambiamos la importación de Telegram a Discord
+import DiscordModal from './components/DiscordModal'; 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
@@ -14,56 +14,56 @@ import NotFound from './pages/NotFound/NotFound';
 import ServiceUnavailable from './pages/ServiceUnavailable/ServiceUnavailable';
 
 function App() {
-    const [showTelegram, setShowTelegram] = useState(true);
+    // Estado para controlar la visibilidad del modal de Discord
+    const [showDiscord, setShowDiscord] = useState(false);
 
     useEffect(() => {
-      setShowTelegram(true);
+      // Se activa el modal al cargar la aplicación
+      setShowDiscord(true);
     }, []);
 
-    const handleCloseTelegram = () => setShowTelegram(false);
+    const handleCloseDiscord = () => setShowDiscord(false);
 
   return (
     <HelmetProvider>
       <Router>
-        <TelegramModal show={showTelegram} onClose={handleCloseTelegram} />
+        {/* Usamos el nuevo componente DiscordModal */}
+        <DiscordModal show={showDiscord} onClose={handleCloseDiscord} />
+        
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* Letter routes */}
+          
+          {/* Rutas de Categorías y Letras */}
           <Route path="/letter/:letter" element={<Category />} />
           <Route path="/letter/:letter/page/:page" element={<Category />} />
-          {/* Genre routes */}
           <Route path="/category/genre/:genreName" element={<Category />} />
           <Route path="/category/genre/:genreName/page/:page" element={<Category />} />
-          {/* Language routes */}
           <Route path="/category/language/:languageName" element={<Category />} />
           <Route path="/category/language/:languageName/page/:page" element={<Category />} />
-          {/* Category type routes */}
           <Route path="/category/type/:categoryName" element={<Category />} />
           <Route path="/category/type/:categoryName/page/:page" element={<Category />} />
-          {/* Network routes (nested) */}
           <Route path="/category/network/:networkName" element={<Category />} />
           <Route path="/category/network/:networkName/page/:page" element={<Category />} />
-          {/* Franchise routes (nested) */}
           <Route path="/category/franchise/:franchiseName" element={<Category />} />
           <Route path="/category/franchise/:franchiseName/page/:page" element={<Category />} />
-          {/* General category routes */}
           <Route path="/category/:categoryName" element={<Category />} />
           <Route path="/category/:categoryName/page/:page" element={<Category />} />
-          {/* Movies routes */}
+          
+          {/* Rutas de Películas */}
           <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/" element={<Movies />} />
           <Route path="/movies/page/:page" element={<Movies />} />
-          {/* Series routes */}
+          
+          {/* Rutas de Series */}
           <Route path="/series" element={<Series />} />
-          <Route path="/series/" element={<Series />} />
           <Route path="/series/page/:page" element={<Series />} />
-          {/* Details routes for both series and movies */}
+          
+          {/* Detalles y Episodios */}
           <Route path="/series/:id" element={<Details />} />
           <Route path="/movies/:id" element={<Details />} />
-          {/* Episode route */}
           <Route path="/episode/:id" element={<Episode />} />
-          {/* Error pages */}
+          
+          {/* Páginas de Error */}
           <Route path="/503" element={<ServiceUnavailable />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
